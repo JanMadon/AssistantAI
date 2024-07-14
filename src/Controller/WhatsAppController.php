44fc8f82f926d5110ac554c2965a75c2307e58e4
Jan <jan.madon@gmail.com>
2024-07-14 12:37:28 +0200

    rename whatsapp controller

--- src/Controller/WhatsAppController.php
+++ src/Controller/WhatsAppController.php
@@ -2,18 +2,28 @@
 
 namespace App\Controller;
 
+use App\Service\WhatsAppService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-class APIwhatsapp extends AbstractController
+class WhatsAppController extends AbstractController
 {
+    private WhatsAppService $whatsAppService;
+
+    public function __construct(WhatsAppService $whatsAppService)
+    {
+        $this->whatsAppService = $whatsAppService;
+    }
 
     #[Route('/api/whatsApp/{number}', 'whatsApp')]
     public function getMessages(Request $request, int $number): Response
     {
+        // sprawdz sesje:
+        
+
         // GET
         $endpoint = '/api/messages';
 
