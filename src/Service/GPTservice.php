2dbc7bbe19506c5a77ba778261b9402a5fa589c4
Jan <jan.madon@gmail.com>
2024-08-16 21:36:19 +0200

    adaptation api openAI request to symfony

--- src/Service/GPTservice.php
+++ src/Service/GPTservice.php
@@ -2,71 +2,77 @@
 
 namespace App\Service;
 
+use Couchbase\HttpException;
+use phpDocumentor\Reflection\Types\This;
 use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
+use Symfony\Component\HttpClient\Exception\ClientException;
+use Symfony\Component\HttpKernel\Exception\HttpException as SymfonyHttpException;
 
-class GPTservice {
- 
+;
+
+use Symfony\Contracts\HttpClient\Exception\ClientExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\HttpExceptionInterface;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+
+class GPTservice
+{
+
+    private string $url = 'https://api.openai.com/v1/chat/completions';
     //private string $model;
     private ParameterBagInterface $config;
+    private HttpClientInterface $httpClient;
 
-    public function __construct(ParameterBagInterface $config)
+
+    public function __construct(ParameterBagInterface $config, HttpClientInterface $httpClient)
     {
         //$this->model = $model;
         $this->config = $config;
+        $this->httpClient = $httpClient;
     }
 
-    public function prompt(string $model, string $system, array|string $contents)
+    public function prompt(string $model, string $system, array $contents)
     {
-        $contents = $this->prepareConverstionArray($contents);
+        $contents = $this->prepareConversationArray($contents);
+
+        //$model = 'gpt-3.5-turbo';
+        //$model = 'gpt-4';
+        $payload = [
+            'model' => $model,
+            'messages' => [
+                [
+                    'role' => 'system',
+                    'content' => $system
+                ],
+                ...$contents
+            ]
+        ];
 
         try {
-            $userContent = [];
-            if (is_string($contents)) {
-                $userContent[] = [
-                    'role' => 'user',
-                    'content' => $contents
-                ];
-                $contents =  $userContent;
-            }
-
-            //$model = 'gpt-3.5-turbo';
-            //$model = 'gpt-4';
-            $payload = [
-                'model' => $model,
-                'messages' => [
-                    [
-                        'role' => 'system',
-                        'content' => $system
-                    ],
-                    ...$contents
+            $response = $this->httpClient->request('POST', $this->url, [
+                'json' => $payload, // symfony zam konwertuje na jsona
+                'headers' => [
+                    'Content-Type' => 'application/json',
+                    'Accept' => 'application/json',
+                    'Content-Length: ' . strlen(json_encode($payload)),
+                    'Authorization' => 'Bearer ' . $this->config->get('API_KEY_OPENAI')
                 ]
-            ];
-
-            $payload = json_encode($payload);
-
-            $curl = curl_init('https://api.openai.com/v1/chat/completions');
-            curl_setopt($curl, CURLOPT_POST, true);
-            curl_setopt($curl, CURLOPT_POSTFIELDS, $payload);
-            curl_setopt($curl, CURLOPT_HTTPHEADER, [
-                'Content-Type: application/json',
-                'Content-Length: ' . strlen($payload),
-                'Authorization: Bearer ' . $this->config->get('API_KEY_OPENAI')
             ]);
-            curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-
-            $response = curl_exec($curl);
-            $response = json_decode($response)->choices[0]->message->content;
-            curl_close($curl);
-        } catch (\Exception $err) {
-            $response = $err->getMessage();
+            $response = json_decode($response->getContent(false))->choices[0]->message->content;
+        } catch (ClientException $exception) {
+            $response = $exception->getMessage();
+        }  catch (HttpExceptionInterface $exception) {
+            $response = $exception->getMessage();
+        } catch (\Exception $exception) {
+            $response = 'Unexpected error: ' . $exception->getMessage();
         }
+
         return $response;
     }
 
-    private function prepareConverstionArray(array $converstion): array
+    private function prepareConversationArray(array $conversation): array
     {
         $preparedConversation = [];
-        foreach ($converstion as $messages) {
+        foreach ($conversation as $messages) {
             if (array_keys($messages)[0] === 'User') {
                 $preparedConversation[] = [
                     'role' => 'user',
