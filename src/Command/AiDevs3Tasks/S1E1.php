70b6017ff5a9fd59f348839df79600f0780f6a8d
Jan <jan.madon@gmail.com>
2025-01-19 22:01:09 +0100

    extract s1e1 from controller to command execute

---
+++ src/Command/AiDevs3Tasks/S1E1.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Command\AiDevs3Tasks;
+
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+
+#[AsCommand(name: 'app:S1e1', description: 'Week 1 / task monday')]
+class S1E1 extends BaseCommand
+{
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $htmlDomContent = file_get_contents($this->aiDevs3Endpoint['S1E1_LOGIN']);
+        $dom = new DOMDocument();
+        $dom->loadHTML($htmlDomContent);
+        $question = substr($dom->getElementById('human-question')->textContent, 9);
+
+        $answerGPT = $this->GPTservice->prompt('Podaj rok jako cyfrę nic więcej nie zwracaj', $question, 'gpt-4o-mini');
+
+        $responseAiDevs = $this->httpClient->request(
+            'POST',
+            $this->AiDevs3Endpoint['S1E1_LOGIN'], [
+                'headers' => ['Content-Type' => 'application/x-www-form-urlencoded'],
+                'body' => [
+                    'username' => 'tester',
+                    'password' => '574e112a',
+                    'answer' => (int)$answerGPT,
+                ]
+            ]
+        );
+
+        dump($responseAiDevs);
+
+        return Command::SUCCESS;
+    }
+
+}
